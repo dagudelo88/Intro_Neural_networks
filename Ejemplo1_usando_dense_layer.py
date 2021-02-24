83a85572fc6da60f17e0be0fb70b5d4d2f5e39cf
Daniel Agudelo <dagudelo88@gmail.com>
2021-02-24 05:59:37 -0500

    Soft mas en dense layer class

--- Ejemplo1_usando_dense_layer.py
+++ Ejemplo1_usando_dense_layer.py
@@ -1,20 +1,26 @@
 import numpy as np
 import nnfs
-from dense_layer_class import Layer_Dense, Activation_ReLU
+from dense_layer_class  import  Layer_Dense, Activation_ReLU, Activation_Softhmax
 from nnfs.datasets import spiral_data
 
 nnfs.init()
 
 # create data set
-
 x, y = spiral_data(samples=100, classes= 3)
 
 #Create dense layer with 2 input features and 3 output values 
-
 dense1 = Layer_Dense(2,3)
 
 # Create ReLU activation (to be used with Dense layer)
 activation1 = Activation_ReLU()
+
+# Create second dense layer with 3 input features
+# (as we take output of previus layer here) and 3 outputs values
+dense2 = Layer_Dense(3,3)
+
+# Create a Softmax activation( to be used with Dense layer)
+activation2 = Activation_Softhmax()
+
 
 # Performe a forward  pass of our training data through this layer
 dense1.forward(x)
@@ -23,4 +29,14 @@ dense1.forward(x)
 # Takes in output from previus layer
 activation1.forward(dense1.output)
 
-print(activation1.output[:5])
+# make a forward pass through second Dense layer
+# it takes the output of second dense layer here
+dense2.forward(activation1.output)
+
+
+# Make a forward pass through activation funtion 
+# it takes the output of second dense layer here
+activation2.forward(dense2.output)
+
+
+print(activation2.output[:5])
--- dense_layer_class.py
+++ dense_layer_class.py
@@ -19,4 +19,16 @@ class Activation_ReLU:
     def forward(self,inputs):
         self.output = np.maximum(0, inputs)
 
-        
+
+# Softmax activation
+class Activation_Softhmax:
+    # Forward pass
+    def forward(self,inputs):
+
+        # Get unnormalized probabilities
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True)) 
+
+        # Normalized them for each sample
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+        self.output = probabilities    
